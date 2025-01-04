d8317b48f8ae9f13af8d95afdde4a40a24c72e2d
Ashutosh Negi <ssbruh200@gmail.com>
2025-01-04 22:57:21 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs,not browser
+
+//Code Readbility Matters
+console.log(3+3)
+
+console.log("Ashutosh")
+
+
+
+let name = "ashutosh"
+let age = 19
+let isLoggedIn = false
+
+
+// number(range) => 2^53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value-> representation of empty value
+// undefined => A value that is not defined at the moment
+// symbol => unique
+
+//object
+
+console.log(typeof "Ashutosh");
+
+console.log(typeof null);// object
+console.log(typeof NaN);//Number
+console.log(typeof undefined);//undefined
+
+
---
+++ 03_datatypesConversion.js
@@ -0,0 +1,67 @@
+// let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN not a number
+true => 1
+false => 0
+
+*/
+
+// let isLoggedIn = ""
+
+// let booleanIsLogged = Boolean(isLoggedIn)
+// console.log(booleanIsLogged);
+/*
+ 1 => true; 0 =>false
+ "" => false
+ "Ashutosh" =>true
+*/
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//*********** Operations***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2**2)
+// console.log(2*2)
+// console.log(2+2)
+// console.log(2/2)
+// console.log(2-2)
+// console.log(2%2)
+
+// let str1 = "Ashutosh"
+// let str2 = "Negi"
+// let str4 = " "
+// let str3 = str1+str4+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+
+// console.log(-true);
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter) 
+--gameCounter
+console.log(gameCounter)
+
