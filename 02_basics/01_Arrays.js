45eec93c014ed73895b27ad0ba7128aa837bd132
high-command-dev <ssbruh200@gmail.com>
2025-01-08 00:21:11 +0530

    arrays completed

--- 02_basics/01_Arrays.js
+++ 02_basics/01_Arrays.js
@@ -1,2 +1,37 @@
-const arr = ["hello", "world", "how", "are", "you"];
-console.log(arr) // []
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr[0]); // []
+// const myHero = ["Ashutosh", "SpiderMan"]
+const myArr = new Array(1,2,3,4);
+// console.log(myArr[2]);
+// console.log(myHero[0]);
+// console.log(arr[1]);
+
+// Array methods
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop();
+
+// myArr.unshift(9)
+// myArr.unshift(6) //Added to the first.
+// myArr.shift(); //removed the shifted operation.
+// myArr.shift();
+// console.log(myArr.includes(9));
+// const newArr = myArr.join();//join->convert it to string
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+console.log("A ",myArr);
+const myn2 = myArr.splice(1, 3)
+
+console.log(myn2);
+console.log("B ",myArr);
+
---
+++ 02_basics/02_ArraysII.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman", "flash","batman"]
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const  arr = marvel_heros.concat(dc_heros)
+// console.log(arr);
+// This is spread operator '...'
+// const all_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_heros);
+
+// const arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// const real_arr = arr.flat(Infinity)
+
+// console.log(real_arr);
+
+console.log(Array.isArray("Ashutosh"));
+
+console.log(Array.from("Ashutosh"));
+
+console.log(Array.from({name: "Ashutosh"})); // Interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
+
+asc = asc
