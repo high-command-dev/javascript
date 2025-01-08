a4d48fcd690ced34240a4ffd3a14d70068f2344a
high-command-dev <ssbruh200@gmail.com>
2025-01-09 00:00:29 +0530

    objects modifies with symbols

--- 03_objects/objects.js
+++ 03_objects/objects.js
@@ -1,5 +1,23 @@
+const mySym = Symbol("key1");
 const jsUser = {
-  name:"Ashutosh",
+  "full name":"Ashutosh",
+  age: 19,
+  mySym : "mykey1",
+  location: "India",
+  email: "ashutosh@oracle.com",
+  isLoggedIn: true,
+  lastLoginDays: ["monday","tuesday","wednesday"],
 }
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(typeof jsUser.mySym);
+
+
+
+
+
+
 
 //myArray = ["h","e","l","l","o"]
